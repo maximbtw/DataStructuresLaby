0c2d48df34ae094b71472c131ebee42ea7b97eff
maximbtw <traglodet2001@gmail.com>
2021-03-21 17:16:31 +0300

    Обновил бинарный поиск

--- DataStructuresLaby/Lab2/BinarySearch.cs
+++ DataStructuresLaby/Lab2/BinarySearch.cs
@@ -4,19 +4,17 @@ namespace DataStructuresLaby.Lab2
 {
     class BinarySearch<TValue> where TValue : IComparable
     {
-        public static int FindLeftIndex(TValue[] arr, TValue value)
-        {
-            return BinSearchLeftBorder(arr, value, 0, arr.Length -1);
-        }
+        public static int Search(TValue[] arr, TValue value)
+                       => Search(arr, value, 0, arr.Length - 1);
 
-        private static int BinSearchLeftBorder(TValue[] array, TValue value, int left, int right)
+        private static int Search(TValue[] array, TValue value, int left, int right)
         {
             if (left >= right)
                 return (array[right].CompareTo(value) == 0) ? right : -1;
             var middle = (right + left) / 2;
             if (array[middle].CompareTo(value) < 0)
-                return BinSearchLeftBorder(array, value, middle + 1, right);
-            return BinSearchLeftBorder(array, value, left, middle);
+                return Search(array, value, middle + 1, right);
+            return Search(array, value, left, middle);
         }
     }
 }
